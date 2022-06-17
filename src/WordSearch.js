d0060545a79c35594302c198e1fddf24d482f7ba
Diksha <dikshapatidar0149@gmail.com>
2022-06-17 19:22:27 +0530

    data fetched from api

--- src/WordSearch.js
+++ src/WordSearch.js
@@ -1,10 +1,17 @@
 import React,{useState} from "react";
+import axios from "axios";
 export default function WordSearch(){
     let [keyword,setKeyword]=useState("");
+    function handlereasponse(reasponse){
+         console.log(reasponse);
+    }
     function Search(event){
         event.preventDefault();
         alert(keyword);
+        let apiurl=`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiurl).then(handlereasponse);
     }
+    // https://api.dictionaryapi.dev/api/v2/entries/en/{keyword}
     function handlekeywordchange(event){
          setKeyword(event.target.value);
     }
